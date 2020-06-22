2cf84401550f59868b988ceb86adc29aa006cd8d
Leonid Prusov <39962817+SpiderQshka@users.noreply.github.com>
2020-06-22 13:56:16 +0300

    Updates

---
+++ precache-manifest.eb0b153688356d60299db091feea953a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21e0a172e54ceb54dea028d1b5ef0f96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d8658d61b0ad31010a6",
+    "url": "/static/css/main.f708335a.chunk.css"
+  },
+  {
+    "revision": "d5292cf6773a3e5f5c54",
+    "url": "/static/js/2.8197b171.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8197b171.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d8658d61b0ad31010a6",
+    "url": "/static/js/main.c1c7c0e3.chunk.js"
+  },
+  {
+    "revision": "9271a0544bbaa8a9afe7",
+    "url": "/static/js/runtime-main.7c55928e.js"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/static/media/OpenSans-Regular.3ed9575d.ttf"
+  }
+]);
