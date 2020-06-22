b24210b4a3686b0f2747d5170847e058340c3637
Leonid Prusov <39962817+SpiderQshka@users.noreply.github.com>
2020-06-22 14:20:02 +0300

    Updates

---
+++ precache-manifest.c42c7b7fe66def467b140e3e5d5d54df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91508a980b1046dc4330d831b663fc0a",
+    "url": "/SpiderQshka.github.io/index.html"
+  },
+  {
+    "revision": "ffeebad3843752c5c187",
+    "url": "/SpiderQshka.github.io/static/css/main.b5c80e3c.chunk.css"
+  },
+  {
+    "revision": "d5292cf6773a3e5f5c54",
+    "url": "/SpiderQshka.github.io/static/js/2.8197b171.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SpiderQshka.github.io/static/js/2.8197b171.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffeebad3843752c5c187",
+    "url": "/SpiderQshka.github.io/static/js/main.4e2c5c73.chunk.js"
+  },
+  {
+    "revision": "bb7858960078561b21e7",
+    "url": "/SpiderQshka.github.io/static/js/runtime-main.a00d3a42.js"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/SpiderQshka.github.io/static/media/OpenSans-Regular.3ed9575d.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.eb0b153688356d60299db091feea953a.js"
+  "/SpiderQshka.github.io/precache-manifest.c42c7b7fe66def467b140e3e5d5d54df.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SpiderQshka.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
